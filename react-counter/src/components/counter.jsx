4aebc1757b4bd0b6e67fdc655509cc81db7d395c
mariolazzari <mario.lazzari@gmail.com>
2019-04-22 12:45:57 +0200

    Done lesson 30

---
+++ react-counter/src/components/counter.jsx
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  render() {
+    const { tags } = this.state;
+
+    return (
+      <Fragment>
+        <span style={{ fontSize: 30 }} className={this.getCountClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary sm-2">Increment</button>
+        {tags.length === 0 ? (
+          <p>No items available</p>
+        ) : (
+          <ul>
+            {tags.map((tag, i) => (
+              <li key={i}>{tag}</li>
+            ))}
+          </ul>
+        )}
+      </Fragment>
+    );
+  }
+
+  getCountClasses() {
+    const { count } = this.state;
+    let classes = "badge m-2 badge-";
+    classes += count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
